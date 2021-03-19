f9621716b742bd14613d0493bfb8f8639818f239
luciano <lucianogrilli03@gmail.com>
2021-03-19 15:38:49 -0300

    metodo findByTituloOrResumen arreglado

--- microservicios-empresa/src/main/java/com/formaciondbi/microservicios/empresa/repository/NoticiaRepository.java
+++ microservicios-empresa/src/main/java/com/formaciondbi/microservicios/empresa/repository/NoticiaRepository.java
@@ -16,10 +16,10 @@ public interface NoticiaRepository extends Repository<Noticia, Long>{
 	public List<Noticia> findNoticiasByEmpresaId(Long id); 
 	
 	//trae una consulta descendente con limite 5(el limite da error, despues arreglar)
-	@Query("select n from Empresa e join e.noticias n where e.id=?1 AND upper(n.titulo) like upper(concat('%', ?2, '%')) or upper(n.resumen) like upper(concat('%', ?1, '%')) ORDER BY n.createAt DESC")
+	@Query("select n from Empresa e join e.noticias n where e.id=?1 AND upper(n.titulo) like upper(concat('%', ?2, '%')) or upper(n.resumen) like upper(concat('%', ?2, '%')) ORDER BY n.createAt DESC")
 	public List<Noticia> findByTituloOrResumen(Long id, String term);
 	
 	//trae una consulta descendente pageable
-	@Query("select n from Empresa e join e.noticias n where e.id=?1 AND upper(n.titulo) like upper(concat('%', ?2, '%')) or upper(n.resumen) like upper(concat('%', ?1, '%')) ORDER BY n.createAt DESC")
+	@Query("select n from Empresa e join e.noticias n where e.id=?1 AND upper(n.titulo) like upper(concat('%', ?2, '%')) or upper(n.resumen) like upper(concat('%', ?2, '%')) ORDER BY n.createAt DESC")
 	public Page<Noticia> findAllPageableByEmpresaId(Long id,Pageable pageable);
 }
